186227f4e17a9f6c8f12d88eaee66ffdf41f7c9d
Виктор Балашов <viktor.d.balashov@gmail.com>
2026-02-15 14:10:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ print("**this is a passvords bot**")
 elements = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 pass_length = int(input("**Enter pass length: "))
 password = ""
-
 for i in range(pass_length):
     password += random.choice(elements)
 print("**your top secret code**")
